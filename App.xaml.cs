c64125cf817b8f223debec954195a4df3a2d678a
BnyioM <brunomaia.duarte@gmail.com>
2023-04-18 12:58:55 -0300

    APP.xaml.cs modificado

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using ITAssetsManagement.Data;
+using ITAssetsManagement;
 
-namespace ITAssetsManagement
+namespace wpf_catalog
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new();
+
+            services.AddDbContext<Context>(options =>
+            {
+                options.UseSqlServer("Data source=BRUNOPC\\SQLEXPRESS; initial catalog=IT_Assets_Management_Relational; user id=sa; pwd=bruno123;");
+            });
+
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+
+            mainWindow.Show();
+        }
     }
 }
